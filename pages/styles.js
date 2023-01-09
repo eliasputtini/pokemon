bccd69a0472040e2e291a922769ccd8bfd791526
Elias Puttini da Cunha <elias_puttini@outlook.com>
2023-01-09 03:15:12 -0300

    estudos

--- pages/styles.js
+++ pages/styles.js
@@ -3,35 +3,13 @@ import styled from "styled-components";
 
 const CardContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  width: 300px;
-  height: 400px;
-  background-color: white;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  border-radius: 5px;
-  padding: 20px;
 `;
 
-const CardTitle = styled.h3`
-  font-size: 24px;
-  color: black;
-  font-weight: bold;
-  margin: 0;
-`;
+const CardTitle = styled.h3``;
 
-const CardImage = styled.img`
-  width: 100%;
-  height: 200px;
-  object-fit: cover;
-  border-radius: 5px;
-`;
+const CardImage = styled.img``;
 
-const CardDescription = styled.p`
-  font-size: 16px;
-  margin: 20px 0;
-`;
+const CardDescription = styled.p``;
 
 const Card = ({ title, imageUrl, description }) => (
   <CardContainer>
